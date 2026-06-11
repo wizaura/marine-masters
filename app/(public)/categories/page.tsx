4f0f3cbfc7b09ca895bef0e1e361ad1ffeed259e
Melbin Mathew <melbinppmathewp@gmail.com>
2026-06-11 21:06:33 +0530

    1st commit - Added home page 1.1

--- app/(public)/categories/page.tsx
+++ app/(public)/categories/page.tsx
@@ -5,7 +5,7 @@ export default function CategoriesPage() {
     return (
         <div>
             <CategoriesHero />
-            <CategoriesList />
+            {/* <CategoriesList /> */}
         </div>
     )
 }
